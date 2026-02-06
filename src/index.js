ee5f268ba0d676b62b419430e023d2b54ed84639
Mohamed-Fasidh <115717348+Mohamed-Fasidh@users.noreply.github.com>
2026-02-06 09:35:45 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,9 +5,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const PORT = process.env.PORT || 5000;
 const app = express();
-app.use(cors({
-  origin:'*'
-}));
+app.use(cors());
 app.use(express.json());
 
 
